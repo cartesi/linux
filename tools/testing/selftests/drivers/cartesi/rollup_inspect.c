8f6aa18b70a2797275c8e92a4050d7c3e3382993
Marcelo Politzer <marcelo.politzer@cartesi.io>
2022-04-08 13:39:56 -0300

    First ioctl must be either 'finish' or 'throw_exception'

--- tools/testing/selftests/drivers/cartesi/rollup_inspect.c
+++ tools/testing/selftests/drivers/cartesi/rollup_inspect.c
@@ -7,33 +7,45 @@
 #define ROLLUP_DEVICE_NAME "/dev/rollup"
 
 FIXTURE(rollup) {
-	int fd;
+    int fd;
 };
 FIXTURE_SETUP(rollup) {
-	self->fd = open(ROLLUP_DEVICE_NAME, O_RDWR);
-	ASSERT_GT(self->fd, 0) {
-		TH_LOG("fixture error: %s\n", strerror(self->fd));
-	}
+    self->fd = open(ROLLUP_DEVICE_NAME, O_RDWR);
+    ASSERT_GT(self->fd, 0) {
+        TH_LOG("fixture error: %s\n", strerror(self->fd));
+    }
 }
 FIXTURE_TEARDOWN(rollup) {
-	close(self->fd);
+    close(self->fd);
 }
 TEST_F(rollup, voucher_and_notice_must_fail_on_inspect) {
-	int ret = 0;
+    int ret = 0;
 
-	struct rollup_finish finish = {
-		.accept_previous_request = true,
-	};
-    uint8_t msg[] = "invalid";
-	struct rollup_voucher voucher = {
+    struct rollup_finish finish = {
+        .accept_previous_request = true,
+    };
+    uint8_t msg[] = "payload";
+    struct rollup_voucher voucher = {
         .payload = {msg, sizeof(msg)-1},
-	};
+    };
     struct rollup_notice notice = {
         .payload = {msg, sizeof(msg)-1},
     };
     struct rollup_report report = {
         .payload = {msg, sizeof(msg)-1},
     };
+
+    /* fail to emit a voucher before the first finish */
+    ASSERT_EQ(ioctl(self->fd, IOCTL_ROLLUP_WRITE_VOUCHER, (unsigned long) &voucher), -1);
+    ASSERT_EQ(errno, EBADE);
+
+    /* fail to emit a notice before the first finish */
+    ASSERT_EQ(ioctl(self->fd, IOCTL_ROLLUP_WRITE_NOTICE, (unsigned long) &notice), -1);
+    ASSERT_EQ(errno, EBADE);
+
+    /* fail to emit a report before the first finish */
+    ASSERT_EQ(ioctl(self->fd, IOCTL_ROLLUP_WRITE_REPORT, (unsigned long) &report), -1);
+    ASSERT_EQ(errno, EBADE);
 
     ASSERT_EQ(
         (ret = ioctl(self->fd, IOCTL_ROLLUP_FINISH, (unsigned long) &finish)) ||
