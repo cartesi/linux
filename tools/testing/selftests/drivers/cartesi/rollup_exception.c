c245d17e21e1f2c4e05d3ad63132d0d6913ab7d5
Marcelo Politzer <marcelo.politzer@cartesi.io>
2022-04-08 13:39:56 -0300

    First ioctl must be either 'finish' or 'throw_exception'

--- tools/testing/selftests/drivers/cartesi/rollup_exception.c
+++ tools/testing/selftests/drivers/cartesi/rollup_exception.c
@@ -7,36 +7,28 @@
 #define ROLLUP_DEVICE_NAME "/dev/rollup"
 
 FIXTURE(rollup) {
-	int fd;
+    int fd;
 };
 FIXTURE_SETUP(rollup) {
-	self->fd = open(ROLLUP_DEVICE_NAME, O_RDWR);
-	ASSERT_GT(self->fd, 0) {
-		TH_LOG("fixture error: %s\n", strerror(self->fd));
-	}
+    self->fd = open(ROLLUP_DEVICE_NAME, O_RDWR);
+    ASSERT_GT(self->fd, 0) {
+        TH_LOG("fixture error: %s\n", strerror(self->fd));
+    }
 }
 FIXTURE_TEARDOWN(rollup) {
-	close(self->fd);
+    close(self->fd);
 }
 TEST_F(rollup, exception) {
-	int ret;
-	const char msg[] = "exception";
+    char msg[] = "exception";
 
-	struct rollup_finish finish = {
-		.accept_previous_request = true,
-	};
-	struct rollup_exception exception = {
-		.payload = {
-			.data = msg,
-			.length = sizeof(msg)-1,
-		},
-	};
+    struct rollup_exception exception = {
+        .payload = {
+            .data = msg,
+            .length = sizeof(msg)-1,
+        },
+    };
 
-	if ((ret = ioctl(self->fd, IOCTL_ROLLUP_FINISH, (unsigned long) &finish)) ||
-	    (ret = ioctl(self->fd, IOCTL_ROLLUP_THROW_EXCEPTION, (unsigned long) &exception)))
-		return ret;
-
-	return 0;
+    ASSERT_EQ(ioctl(self->fd, IOCTL_ROLLUP_THROW_EXCEPTION, (unsigned long) &exception), 0);
 }
 
 TEST_HARNESS_MAIN
